4a9a00817c722528356b47bfff4833143bdb1f0d
kimhyunjoong <100519998+khj0196@users.noreply.github.com>
2025-03-25 20:03:49 +0900

    feat: 금액 입력 받기 및 검증

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,10 @@
 package lotto;
 
+import lotto.domain.InputMoney;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputMoney inputMoney = new InputMoney();
+        inputMoney.input();
     }
 }
